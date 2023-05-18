04eccf250434fc054febbc51df9f3fac0580a13d
Shuhei Shibahara <Shoehey06@gmail.com>
2023-05-18 09:07:11 -0700

    add patient

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 
+
+
+
 class Doctor:
-  def __init__(self, _name, _email, _workout_list):
+  def __init__(self, _name, _email):
     self.name = _name
     self.email = _email
-    self.workout_list = _workout_list
+    self.workout_list = {}
+    self.patients = {}
+
+  def add_patient(self, patient):
+    self.patients[patient.id] = patient
+
+
+class Patient:
+  def __init__(self, _name, _email):
+    self.name = _name
+    self.email = _email
+    self.workout_list = {}
+    self.doctor = None
+
 
   
