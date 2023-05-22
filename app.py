004df158351c3b71082eda2b8f10f5c380aa887e
Shuhei Shibahara <Shoehey06@gmail.com>
2023-05-22 09:42:15 -0700

    doctors pateint and exercise

--- app.py
+++ app.py
@@ -3,10 +3,33 @@
 class Doctor:
     def __init__(self, name, email):
         self.name = name
-        self.patients = []
-        self.exercises = []
-        self.patient_id_counter = 1
-        self.exercise_id_counter = 1
+        self.email = email
+
+    def assign_exercises(self, patient, exercises):
+        patient.assign_exercises(self, exercises)
+
+
+class Patient:
+    def __init__(self, name):
+        self.name = name
+        self.exercise_list = {}
+
+    def assign_exercises(self, doctor, exercises):
+      if self.exercise_list[doctor]:
+        self.exercise_list[doctor].append(exercises)
+      else:
+        self.exercise_list[doctor] = []
+
+    def get_exercises(self):
+        all_exercises = []
+        for exercises in self.exercise_list.values():
+            all_exercises.extend(exercises)
+        return all_exercises
+
+
+class Exercise:
+    def __init__(self, name):
+        self.name = name
 
 
 
