222d094fdd7f32fea8d1284ac240a4f705af9314
Shuhei Shibahara <Shoehey06@gmail.com>
2023-05-22 09:27:20 -0700

    trial

--- app.py
+++ app.py
@@ -6,12 +6,15 @@ class Doctor:
   def __init__(self, _name, _email):
     self.name = _name
     self.email = _email
-    self.workout_list = {}
-    self.patients = {}
+    self.workout_list = []
+    self.patients = []
 
   def add_patient(self, patient):
     self.patients[patient.id] = patient
+  
+  def add_exercise(self, exercise):
 
+        self.patients[patient.id] = patient
 
 class Patient:
   def __init__(self, _name, _email):
@@ -19,6 +22,10 @@ class Patient:
     self.email = _email
     self.workout_list = {}
     self.doctor = None
+
+class Exercise:
+  def __init__(self) -> None:
+    pass
 
 
   
